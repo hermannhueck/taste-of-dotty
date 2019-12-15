285c52fcd978c0ee05b92ab06d81c69d0c8d66f9
Hermann Hueck <hermann.hueck@mailbox.org>
2019-12-15 18:44:54 +0100

    + Export Clauses

---
+++ taste-of-dotty/src/main/scala-0.21/dotty/samples/ExportClauses.scala
@@ -0,0 +1,26 @@
+package dotty.samples
+
+class A
+  def a1 = 42
+  def a2 = a1.toString
+
+class B
+  private val a = new A
+  export a.{a2 => aString} // exports a.a2 aliased to aString
+
+@main def runExports: Unit =
+
+  import scala.util.chaining._
+  import scala.language.implicitConversions
+  import util._
+
+  lineStart() pipe println
+
+  val b = new B
+
+  // a.a1 and a.a2 are not directly accessible as a is private in B.
+  // The export claus makes a.a2 (alaiased to aString) accessible as a member of b
+  val bString = b.aString ensuring (_ == 42.toString)
+  bString pipe println
+
+  lineEnd() pipe println
