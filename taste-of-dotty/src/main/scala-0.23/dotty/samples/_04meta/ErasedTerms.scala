c0c3571d8b79121836a123e0439beb0f56d741aa
Hermann Hueck <hermann.hueck@mailbox.org>
2020-03-10 12:33:56 +0100

    more metaprograming examples

---
+++ taste-of-dotty/src/main/scala-0.23/dotty/samples/_04meta/ErasedTerms.scala
@@ -0,0 +1,159 @@
+package samples._04meta
+
+/*
+  See:
+  https://dotty.epfl.ch/docs/reference/metaprogramming/erased-terms.html
+*/
+object ErasedTerms {
+
+/*
+  // Why erased terms?
+
+  sealed trait State
+  final class On extends State
+  final class Off extends State
+  
+  @scala.annotation.implicitNotFound("State is must be Off")
+  class IsOff[S <: State]
+  object IsOff {
+    implicit def isOff: IsOff[Off] = new IsOff[Off]
+  }
+  
+  class Machine[S <: State] {
+    def turnedOn(implicit ev: IsOff[S]): Machine[On] = new Machine[On]
+  }
+  
+  val m = new Machine[Off]
+  m.turnedOn
+  // m.turnedOn.turnedOn // ERROR
+  //                   ^
+  //                   State is must be Off
+  
+  // How to define erased terms?
+
+  trait Ev
+  
+  def methodWithErasedEv(erased ev: Ev): Int = 42
+
+  val lambdaWithErasedEv: (erased Ev) => Int = {
+   (erased ev: Ev) => 42
+  }
+
+   // erased parameters will not be usable for computations, though they can be used as arguments to other erased parameters.
+
+   def methodWithErasedInt1(erased i: Int): Int =
+     42
+     // i + 42 // ERROR: can not use i
+  
+  def methodWithErasedInt2(erased i: Int): Int =
+    methodWithErasedInt1(i) // OK
+ 
+  // Not only parameters can be marked as erased, val and def can also be marked with erased.
+  // These will also only be usable as arguments to erased parameters.
+
+  erased val erasedEvidence: Ev = new Ev {}
+  methodWithErasedEv(erasedEvidence)
+  
+  // What happens with erased values at runtime?
+
+  // As erased are guaranteed not to be used in computations, they can and will be erased.
+
+  // becomes def methodWithErasedEv(): Int at runtime
+  // def methodWithErasedEv(erased ev: Ev): Int = ...
+
+  def evidence1: Ev = new Ev {}
+  erased def erasedEvidence2: Ev = new Ev {} // does not exist at runtime
+  erased val erasedEvidence3: Ev = new Ev {} // does not exist at runtime
+
+  // evidence1 is not evaluated and no value is passed to methodWithErasedEv
+  methodWithErasedEv(evidence1)
+
+  // State machine with erased evidence example
+
+  object StateMachineWithErased {
+
+    import scala.annotation.implicitNotFound
+
+    sealed trait State
+    final class On extends State
+    final class Off extends State
+    
+    @implicitNotFound("State is must be Off")
+    class IsOff[S <: State]
+    object IsOff {
+      // def isOff will not be called at runtime for turnedOn, the compiler will only require that this evidence exists
+      implicit def isOff: IsOff[Off] = new IsOff[Off]
+    }
+    
+    @implicitNotFound("State is must be On")
+    class IsOn[S <: State]
+    object IsOn {
+      // erased val isOn will not exist at runtime, the compiler will only require that this evidence exists at compile time
+      erased implicit val isOn: IsOn[On] = new IsOn[On]
+    }
+    
+    class Machine[S <: State] private {
+      // ev will disappear from both functions
+      def turnedOn(given erased ev: IsOff[S]): Machine[On] = new Machine[On]
+      def turnedOff(given erased ev: IsOn[S]): Machine[Off] = new Machine[Off]
+    }
+    
+    object Machine {
+      def newMachine(): Machine[Off] = new Machine[Off]
+    }
+    
+    object Test {
+      def main(args: Array[String]): Unit = {
+        val m = Machine.newMachine()
+        m.turnedOn
+        m.turnedOn.turnedOff
+    
+        // m.turnedOff
+        //            ^
+        //            State is must be On
+    
+        // m.turnedOn.turnedOn
+        //                    ^
+        //                    State is must be Off
+      }
+    }
+  }
+
+  // Note that in Inline we discussed erasedValue and inline matches. erasedValue is implemented with erased,
+  // so the state machine above can be encoded as follows:
+
+  object StateMachineWithErasedValue {
+
+    import scala.compiletime._
+
+    sealed trait State
+    final class On extends State
+    final class Off extends State
+    
+    class Machine[S <: State] {
+      inline def turnOn() <: Machine[On] = inline erasedValue[S] match {
+        case _: Off  => new Machine[On]
+        case _: On   => error("Turning on an already turned on machine")
+      }
+      inline def turnOff() <: Machine[Off] = inline erasedValue[S] match {
+        case _: On  => new Machine[Off]
+        case _: Off   => error("Turning off an already turned off machine")
+      }
+    }
+    
+    object Machine {
+      def newMachine(): Machine[Off] = {
+        println("newMachine")
+        new Machine[Off]
+      }
+    }
+    
+    object Test {
+      val m = Machine.newMachine()
+      val on = m.turnOn()
+      val off = m.turnOn().turnOff()
+      // m.turnOn().turnOn() // error: Turning on an already turned on machine
+    }
+  }
+*/
+}
---
+++ taste-of-dotty/src/main/scala-0.23/dotty/samples/_04meta/Macros.scala
@@ -0,0 +1,99 @@
+package samples._04meta
+
+object Macros {
+
+  // Macros: Quotes and Splices
+
+  import scala.quoted._
+
+  inline def assert(expr: => Boolean): Unit =
+    ${ assertImpl('expr) }
+  
+  def assertImpl(expr: Expr[Boolean])(using QuoteContext) = '{
+    if !$expr
+      throw new AssertionError(s"failed assertion: ${${ showExpr(expr) }}")
+  }
+  
+  // def showExpr(expr: Expr[Boolean])(given QuoteContext): Expr[String] =
+    // '{ "<some source code>" } // Better implementation later in this document
+
+  def showExpr[T](expr: Expr[T])(using QuoteContext): Expr[String] =
+    val code: String = expr.show
+    Expr(code)
+
+  // If e is an expression, then '{e} represents the typed abstract syntax tree representing e.
+  // If T is a type, then '[T] represents the type structure representing T.
+  // The precise definitions of "typed abstract syntax tree" or "type-structure" do not matter for now,
+  // the terms are used only to give some intuition. Conversely, ${e} evaluates the expression e,
+  // which must yield a typed abstract syntax tree or type structure, and embeds the result as an expression
+  // (respectively, type) in the enclosing program.
+
+  // Quotations can have spliced parts in them; in this case the embedded splices are evaluated and embedded
+  // as part of the formation of the quotation.
+
+  // Quotes and splices can also be applied directly to identifiers. An identifier $x starting with a $ that appears inside a quoted expression or type is treated as a splice ${x}. Analogously, an quoted identifier 'x that appears inside a splice is treated as a quote '{x}. See the Syntax section below for details.
+
+  // Quotes and splices are duals of each other. For arbitrary expressions e and types T we have:
+
+  // ${'{e}} = e
+  // '{${e}} = e
+  // ${'[T]} = T
+  // '[${T}] = T
+
+  // Types for Quotations
+
+  // The type signatures of quotes and splices can be described using two fundamental types:
+
+    // Expr[T]: abstract syntax trees representing expressions of type T
+    // Type[T]: type structures representing type T.
+
+  // Quoting takes expressions of type T to expressions of type Expr[T] and it takes types T to expressions of type Type[T]. Splicing takes expressions of type Expr[T] to expressions of type T and it takes expressions of type Type[T] to types T.
+
+  // The two types can be defined in package scala.quoted as follows:
+
+  // package scala.quoted
+
+  // sealed abstract class Expr[+T]
+  // sealed abstract class Type[T]
+
+  // Both Expr and Type are abstract and sealed, so all constructors for these types are provided by the system.
+  // One way to construct values of these types is by quoting, the other is by type-specific lifting operations
+  // that will be discussed later on.
+
+  // Lifting Expressions
+
+  import scala.quoted._
+
+  enum Exp {
+    case Num(n: Int)
+    case Plus(e1: Exp, e2: Exp)
+    case Var(x: String)
+    case Let(x: String, e: Exp, in: Exp)
+  }
+
+  import Exp._
+
+  val exp = Plus(Plus(Num(2), Var("x")), Num(4))
+  val letExp = Let("x", Num(3), exp)
+  
+  /*
+  import scala.quoted.{given, _}
+
+  def compile(e: Exp, env: Map[String, Expr[Int]]): Expr[Int] = e match {
+    case Num(n) =>
+      Expr(n)
+    case Plus(e1, e2) =>
+      '{ ${ compile(e1, env) } + ${ compile(e2, env) } }
+    case Var(x) =>
+      env(x)
+    case Let(x, e, body) =>
+      '{ val y = ${ compile(e, env) }; ${ compile(body, env + (x -> 'y)) } }
+  }
+
+  // Running compile(letExp, Map()) would yield the following Scala code:
+
+  '{ val y = 3; (2 + y) + 4 }
+
+  */
+  
+}
